a2abb8a634b2c2dc60e66c2a2f8a492b788da32e
Gonzalo Ferreyra <gonferreyra@gmail.com>
2023-05-14 18:50:19 -0300

    'added 2 katas begginer'

--- 8.fuel-calculator.js
+++ 8.fuel-calculator.js
@@ -9,41 +9,42 @@ Note
 1 Dollar = 100 Cents
 
 - pasar el precio por litro a centavos, 
-- calcular el descuento, no puede ser mas e 25 centavos, y restarselo al precio por litro
+- calcular el descuento, no puede ser mas de 25 centavos, y restarselo al precio por litro
 -  multiplicar precio por litro en centavos por los litros, y dividir por 100
 
 */
 
-const fuelPrices = (litres, pricePerLitre) => {
+const fuelPrice = (litres, pricePerLitre) => {
   let discount = null;
   let pricePerLitreCents = pricePerLitre * 100;
   let total = null;
-  // switch (true) {
-  //   case litres >= 2 <= 4:
-  //     discount = 5;
-  //     break;
-  //   case litres >= 4 <= 6:
-  //     discount = 1;
-  //     break;
-  //   case litres > 6 <= 8:
-  //     discount = 15 * litres;
-  //     break;
-  //   case litres > 8 <= 10:
-  //     discount = 2 * litres;
-  //     break;
-  //   case litres > 10 <= 12:
-  //     discount = 25 * litres;
-  //     break;
-  //   case litres > 12:
-  //     discount = 25;
-  //     break;
-  //   default:
-  //     break;
-  // }
+  switch (true) {
+    case litres >= 2 && litres < 4:
+      discount = 5;
+      break;
+    case litres >= 4 && litres < 6:
+      discount = 10;
+      break;
+    case litres >= 6 && litres < 8:
+      discount = 15;
+      break;
+    case litres >= 8 && litres < 10:
+      discount = 20;
+      break;
+    case litres >= 10 && litres <= 12:
+      discount = 25;
+      break;
+    case litres > 12:
+      discount = 25;
+      break;
+    default:
+      break;
+  }
 
   total = (pricePerLitreCents - discount) * litres;
   console.log(discount);
-  return total;
+  total = total / 100;
+  return Math.round(total * 100) / 100;
 };
 
-console.log(fuelPrices(5, 1.23));
+console.log(fuelPrice(8, 2.5));
